67ebf5488574ae580b0bc259db1125c715810fc5
geraldo_domingos <geraldoneto@locus.software>
2024-03-07 17:27:04 -0300

    ⏪ rollback: adding new exemples in parses fundamentals

--- fundamentals/parses.dart
+++ fundamentals/parses.dart
@@ -6,4 +6,14 @@ void main(List<String> args) {
   String number2 = "2.0";
   var doubleParsed = double.parse(number2);
   assert(doubleParsed == 2.0);
+
+  String oneAsString = 1.toString();
+  assert(oneAsString == "1");
+
+  String piAsString = 3.873.toStringAsFixed(2);
+  assert(piAsString == "3.87");
+
+  // quando definimos um valor no toStringAsFixed, e o próximo é acima de 5, ele converte para mais.
+  String convertToMore = 3.179.toStringAsFixed(2);
+  assert(convertToMore == "3.18");
 }
