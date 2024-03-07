e26bce54e7bbfad22b8a0b3256818320e29cdad3
geraldo_domingos <geraldoneto@locus.software>
2024-03-07 16:59:24 -0300

    ✨feature: create a strings types fundamentals

---
+++ fundamentals/strings.dart
@@ -0,0 +1,16 @@
+void main(List<String> args) {
+  var s1 = 'Single quotes work well for string literals.';
+  var s2 = "Double quotes work just as well";
+
+  var s3 = 'It\'s easy to scape the string delimiter';
+  var s4 = "It's even easier to use the other delimiter";
+
+  print(s1);
+  print(s2);
+  print(s3);
+  print(s4);
+
+  // RAW String
+  var rawString = r'In a raw string, not event \n gets special treatment.';
+  print(rawString);
+}
