ecaac00737649eb3cb2335d4f3102206a90a8411
geraldo_domingos <geraldoneto@locus.software>
2024-03-07 19:55:57 -0300

    ✨feature: create a constants fundamentals

---
+++ fundamentals/constants.dart
@@ -0,0 +1,13 @@
+void main(List<String> args) {
+  const aConstNum = 0;
+  const aConstBool = true;
+  const aConstString = 'a constant string';
+
+  print(aConstNum);
+  print(aConstBool);
+  print(aConstString);
+
+  print(aConstNum.runtimeType);
+  print(aConstBool.runtimeType);
+  print(aConstString.runtimeType);
+}
